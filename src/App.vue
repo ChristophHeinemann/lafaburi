838e7b7ce2d21a20247681d335b490d83c93cd1d
Christoph Heinemann <christoph@cheinemann.de>
2021-04-16 23:07:44 +0200

    enables and prepares layouts

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,5 @@
 <template>
-  <div>
-    <Navigation></Navigation>
-    <div class="min-h-screen bg-gray-100">
-      <div class="py-10">
-        <div
-          class="sm:px-6 lg:px-8 flex flex-row justify-start"
-        >
-          <Sidebar class="mr-5"></Sidebar>
-          <router-view class="w-full" />
-        </div>
-      </div>
-    </div>
-    <SimpleFooter/>
-  </div>
+  <router-view></router-view>
 </template>
 
 <script>
--- src/routes.js
+++ src/routes.js
@@ -9,17 +9,52 @@ import Policy from "./views/legals/Policy.vue";
 import Kontakt from "./views/Kontakt.vue";
 import NotFound from "./views/NotFound.vue";
 
+import LayoutApp from '@/layouts/LayoutApp.vue'
+
 /** @type {import('vue-router').RouterOptions['routes']} */
 export const routes = [
-  { path: "/", component: Home, meta: { title: "Home" } },
+  {
+    path: "/",
+    component: LayoutApp,
+    children: [
+      {
+        path: '',
+        component: Home,
+      },
+    ],
+    meta: { title: "Home" }
+  },
   {
     path: "/about",
     meta: { title: "About" },
-    component: About,
+    component: LayoutApp,
+    children: [
+      {
+        path: '',
+        component: About,
+      },
+    ],
   },
   { path: "/rooms", component: Rooms },
-  { path: "/user/:username", component: Profile },
-  { path: "/entwickler", component: About },
+  {
+    path: "/user/:username",
+    component: LayoutApp,
+    children: [
+      {
+        path: '',
+        component: Profile,
+      },
+    ],
+  },
+  {
+    path: "/entwickler", component: LayoutApp,
+    children: [
+      {
+        path: '',
+        component: About,
+      },
+    ],
+  },
   { path: "/faq", component: Profile },
   { path: "/become/mod", component: BecomeMod },
   { path: "/become/admin", component: BecomeAdmin },
